2fbc0d0d3093b9f3d1c1fe4646886bdb50034cd3
FBL <fabien.bardouil@gmail.com>
2025-10-22 16:53:15 +0200

    Documentation sur le readme et sur les endpoints de l'api

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 app = FastAPI()
 
-class PredictionData(BaseModel):
+class PredictionRawData(BaseModel):
   heure_supplementaires: Literal[0,1]
   age: int
   FE_cadre: Literal[0,1]
@@ -19,6 +19,20 @@ class PredictionData(BaseModel):
   nombre_experiences_precedentes : int
   annees_dans_le_poste_actuel: int
   annee_experience_totale: int
+
+class PredictionTransformedData(BaseModel):
+  heure_supplementaires: Literal[0,1]
+  age: int
+  FE_ratio_ancienneté:float
+  FE_cadre: Literal[0,1]
+  frequence_deplacement: Literal[1,2,3]
+  FE_duree_moy_exp_precedentes:float
+  FE_ratio_evolution:float
+  niveau_education: Literal[1,2,3,4,5]
+  FE_reste_plus_longtemps: Literal[0,1]
+  poste: Literal['Assistant de Direction','Cadre Commercial','Consultant','Directeur Technique','Manager','Représentant Commercial','Ressources Humaines','Senior Manager','Tech Lead']
+  statut_marital:Literal['Célibataire','Marié(e)','Divorcé(e)']
+
 
 bundle = load("app/model/model_HR_prediction_TECHNOVA.joblib")
 HR_model = bundle['model']
@@ -41,13 +55,22 @@ def FE_reste_plus_longtemps(annees_exp_entreprise,duree_moy_exp_precedentes):
     else:
         return 0
     
+@app.get("/")
+def read_root():
+    return {"message": "Hello Futurisys"}
 
 @app.get('/threshold')
 def get_threshold():
+    """
+    Retourne le seuil du f1 score optimal du modèle 
+    """
     return HR_threshold
 
 @app.get("/features")
 def get_features():
+    """
+    Retourne les variables utilisés par le modèle pour calculé la prédiction
+    """
     preprocessor = HR_model.named_steps['preprocessing']
 
     num_features = preprocessor.transformers_[0][2]
@@ -59,6 +82,9 @@ def get_features():
 
 @app.get('/model-info')
 def get_model_info():
+    """
+    Retourne toutes les informations du modèle (type, valeurs des hyperparamètres, variables utilisées par le modèle, seuil f1 optimisé)
+    """
     model = HR_model.named_steps['model']
     infos_model = {
         'type' : model.__class__.__name__,
@@ -68,24 +94,25 @@ def get_model_info():
     }
     return infos_model
 
-@app.post('/predict')
-def post_prediction(data: PredictionData):
+@app.post('/predict_from_raw_data')
+def post_prediction_from_raw_data(data: PredictionRawData):
 
     """
-    Prédit le départ ou non d'un employé
+    Prédit le départ ou non d'un employé sur la base de données brutes accessibles par les RH
 
     ARGS : un dictionnaire contenant les valeurs des différentes variables 
     {
-        "heure_supplementaires": 1,
-        "age": 0,
-        "annees_dans_l_entreprise": 0
-        "nombre_experiences_precedentes" : 0
-        "annees_dans_le_poste_actuel":0
-        "annee_experience_totale" : 0
-        "FE_cadre": 0,
-        "niveau_education": 1,
-        "poste": "Assistant de Direction",
-        "statut_marital": "Célibataire"
+        "heure_supplementaires": 0,1\n
+        "age": 0-100\n
+        "annees_dans_l_entreprise": int\n
+        "frequence_deplacement": 0(peu), 1(occasionnel), 2(fréquent)\n
+        "nombre_experiences_precedentes" : int\n
+        "annees_dans_le_poste_actuel":int\n
+        "annee_experience_totale" : int\n
+        "FE_cadre": 0,1\n
+        "niveau_education": 1,2,3,4,5\n
+        "poste": 'Assistant de Direction','Cadre Commercial','Consultant','Directeur Technique','Manager','Représentant Commercial','Ressources Humaines','Senior Manager','Tech Lead',\n
+        "statut_marital": 'Célibataire','Marié(e)','Divorcé(e)'\n
     }
 
     RETURNS : La probabilité identifié par le modèle et la prédiction en fonction du seuil optimisé
@@ -101,8 +128,36 @@ def post_prediction(data: PredictionData):
 
     df = pd.DataFrame([data_dict_for_model])
 
-    
+    proba = HR_model.predict_proba(df)[0][1]
+    predict = (proba > HR_threshold)
+    return {'probabilité': round(float(proba),3),
+            'prédiction': bool(predict)}
 
+@app.post('/predict_from_transformed_data')
+def post_prediction_from_transformed_data(data: PredictionTransformedData):
+
+    """
+    Prédit le départ ou non d'un employé sur la base de données déjà calculer (Feature engineering)
+
+    ARGS : un dictionnaire contenant les valeurs des différentes variables 
+    {
+        "heure_supplementaires": 0,1\n
+        "age": 0-100\n
+        "FE_ratio_ancienneté": float\n
+        "FE_cadre": 0,1\n
+        "frequence_deplacement": 0(peu), 1(occasionnel), 2(fréquent)\n
+        "FE_duree_moy_exp_precedentes": float\n
+        "FE_ratio_evolution": float\n
+        "niveau_education": 1,2,3,4,5\n
+        "FE_reste_plus_longtemps": 0,1\n
+        "poste": 'Assistant de Direction','Cadre Commercial','Consultant','Directeur Technique','Manager','Représentant Commercial','Ressources Humaines','Senior Manager','Tech Lead',\n
+        "statut_marital": 'Célibataire','Marié(e)','Divorcé(e)'\n
+    }
+
+    RETURNS : La probabilité identifié par le modèle et la prédiction en fonction du seuil optimisé
+    """
+
+    df = pd.DataFrame([data.dict()])
     proba = HR_model.predict_proba(df)[0][1]
     predict = (proba > HR_threshold)
     return {'probabilité': round(float(proba),3),
